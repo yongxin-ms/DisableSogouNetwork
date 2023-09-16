82a70fcb39b2d2fbfcfe5fdadd491bbfb6d21e52
YongXin <yongxin@microsoft.com>
2023-09-16 17:32:59 +0800

    Prevent adding duplicate rules

--- run.ps1
+++ run.ps1
@@ -3,18 +3,20 @@
         [string] $folderName
     )
 
+    Remove-NetFirewallRule -DisplayName "blocked $folderName via script"
+
     Get-ChildItem -Path $folderName -Recurse *.exe | Select-Object FullName | ForEach-Object -Process {
         New-NetFirewallRule `
             -DisplayName "blocked $folderName via script" `
             -Direction Inbound `
             -Program $_.FullName `
             -Action Block
+
         New-NetFirewallRule `
             -DisplayName "blocked $folderName via script" `
             -Direction Outbound `
             -Program $_.FullName `
             -Action Block
-        #Write-Host $_.FullName
     }
 }
 

fdbcb353eb7db3badeef4c34dde93376406d61e6
YongXin <yongxin@microsoft.com>
2023-09-16 17:09:32 +0800

    powershell version

---
+++ run.ps1
@@ -0,0 +1,21 @@
+﻿function Disable-Network {
+    param (
+        [string] $folderName
+    )
+
+    Get-ChildItem -Path $folderName -Recurse *.exe | Select-Object FullName | ForEach-Object -Process {
+        New-NetFirewallRule `
+            -DisplayName "blocked $folderName via script" `
+            -Direction Inbound `
+            -Program $_.FullName `
+            -Action Block
+        New-NetFirewallRule `
+            -DisplayName "blocked $folderName via script" `
+            -Direction Outbound `
+            -Program $_.FullName `
+            -Action Block
+        #Write-Host $_.FullName
+    }
+}
+
+Disable-Network -folderName "C:\Program Files (x86)\SogouInput"
